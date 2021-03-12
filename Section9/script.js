5685a816b7cd8471c3ae2098bc568d31af1d4a56
yizhiyan1992 <u1207314@utah.edu>
2021-03-11 23:50:02 -0700

    update

--- Section9/script.js
+++ Section9/script.js
@@ -96,3 +96,100 @@ document.querySelector('.buy').addEventListener('click', plan.getModel);
 document
   .querySelector('.buy')
   .addEventListener('click', plan.getModel.bind(plan));
+
+const poll = {
+  registerNewAnswer() {
+    const res = Number(
+      prompt(`What is your favorite programming language?
+        0: JS
+        1: Python
+        2: Rust)
+        3: C++
+        (wtrite option number)`)
+    );
+    if (typeof res === 'number' && res < this.answer.length) {
+      this.answer[res] += 1;
+    }
+    console.log(this.answer);
+  },
+  answer: [0, 0, 0, 0],
+  displayResults(arg = 's') {
+    if (typeof arg === 'string') {
+      console.log(`the result is ${this.answer.join(',')}`);
+    } else {
+      console.log(this.answer);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+//iife
+(function (a, b) {
+  return a + b;
+})(3, 4);
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(passengerCount);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+console.dir(booker);
+console.log(booker);
+
+let f;
+const gg = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+gg();
+f();
+h();
+f();
+console.dir(f);
+// example2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`we are boarding all ${n} passengers`);
+  }, 1000);
+  console.log('will wait in', wait);
+};
+boardPassengers();
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
