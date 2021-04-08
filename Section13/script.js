cfa68b23781e0acbc01273e76eedadc7a15985d7
yizhiyan1992 <u1207314@utah.edu>
2021-04-08 09:10:01 -0600

    update

--- Section13/script.js
+++ Section13/script.js
@@ -294,3 +294,88 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgs.forEach(function (img) {
   imgObserver.observe(img);
 });
+
+//slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+//const slider = document.querySelector('.slider');
+//slider.style.transform = 'scale(0.5)';
+//slider.style.overflow = 'visible';
+let curSlide = 0;
+const maxSlide = slides.length;
+const dotContainer = document.querySelector('.dots');
+
+const createDots = function () {
+  slides.forEach((s, i) => {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide='${i}'></button>`
+    );
+  });
+};
+createDots();
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - slide)}%`;
+  });
+};
+goToSlide(0);
+activateDot(0);
+//next slide
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log(new Date());
+});
+window.addEventListener('load', function () {
+  console.log(new Date());
+});
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log('?');
+  return false;
+});
